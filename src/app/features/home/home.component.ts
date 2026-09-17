2c0985694c821a604a252ef6cad33cded88bcfda
William Ribeiro <william6582@hotmail.com>
2026-09-17 20:10:58 -0300

    Ajuste da home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  menuOpen = false;
+
   constructor(private router: Router) {}
 
-  goPatient() { this.router.navigate(['/patient']); }
-  goPsych()   { this.router.navigate(['/psych/login']); }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const clickedInsideMenu = target.closest('.menu-panel');
+    const clickedMenuButton = target.closest('.menu-button');
+
+    if (!clickedInsideMenu && !clickedMenuButton && this.menuOpen) {
+      this.closeMenu();
+    }
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+
+  goPatient(): void {
+    this.closeMenu();
+    this.router.navigate(['/patient']);
+  }
+
+  goPsych(): void {
+    this.closeMenu();
+    this.router.navigate(['/psych/login']);
+  }
+
+  openInstagram(): void {
+    window.open('https://www.instagram.com/marsonpsicologia/', '_blank');
+  }
+
+  openWhatsApp(): void {
+    window.open('https://wa.me/5511954953003', '_blank');
+  }
 }
